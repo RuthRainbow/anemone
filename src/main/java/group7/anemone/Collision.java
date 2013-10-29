604f83447e3bf7aaec6e6fe049a2ce8bccb13467
Ben Beeson <csukci@viglab-24.(none)>
2013-10-29 15:42:34 +0000

    Collision class hello

---
+++ src/main/java/group7/anemone/Collision.java
@@ -0,0 +1,30 @@
+package group7.anemone;
+
+//Class to hold a collision between an agent and an object, which could be another agent or a wall.
+//(If we have breeding on collision, another agent vs wall matters).
+public class Collision {
+	public static final int TYPE_FOOD = 1;
+	public static final int TYPE_AGENT = 2;
+	public static final int TYPE_ENEMY = 3;
+	
+	private Agent agent;
+	private Object collided;
+	
+	public Collision(Agent ag, Object ob){
+		agent = ag;
+		collided = ob;
+	}
+	
+	public Agent getAgent(){
+		return agent;
+	}
+	public Object getCollidedObject(){
+		return collided;
+	}
+	public int getType(){
+		if(collided instanceof Food) return TYPE_FOOD;
+		if(collided instanceof Agent) return TYPE_AGENT;
+		
+		return -1;
+	}
+}
