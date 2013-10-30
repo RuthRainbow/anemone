c93a981dc402ce1d9b0e35fbde5f3d2d16cd0b89
Ruth King <R.A.King@warwick.ac.uk>
2013-10-30 16:37:05 +0000

    Oops, didn't meant o incluce this file

--- src/main/java/group7/anemone/godtest.java
+++
@@ -1,9 +0,0 @@
-package group7.anemone;
-
-public class godtest {
-  public static void main(String[] args) {
-	  God god = new God();
-	  String str = "abcdef";
-	  System.out.println(god.mutate(str));
-  }
-}
