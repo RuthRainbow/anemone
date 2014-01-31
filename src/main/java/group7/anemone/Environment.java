8a82d7e5202b417a070c6ec08b729aafa179ea26
Danyal Drew <danyaldrew@gmail.com>
2014-01-31 14:51:39 +0000

    (Re)implemented full connectivity in default genomes.

--- src/main/java/group7/anemone/Environment.java
+++ src/main/java/group7/anemone/Environment.java
@@ -325,30 +325,52 @@ public class Environment implements Serializable{
 		 *
 		 * */
 		int total = 0;
+		int numMotorNeurons = 3;
+		int visualFieldSize = Agent.configNumSegments;
+		int numVisualNeurons = visualFieldSize*3;
+		ArrayList<Gene> edges = new ArrayList<Gene>();
 		ArrayList<NeatNode> nodes = new ArrayList<NeatNode>();
+		ArrayList<NeatNode> motorNodes = new ArrayList<NeatNode>();
+		ArrayList<NeatNode> visualNodes = new ArrayList<NeatNode>();
 		
 		/* Motor neurons. */
 		for (int i=0; i<3; i++) {
 			NeatNode node = NeatNode.createRSNeatNode(total++);
-			nodes.add(node);
+			motorNodes.add(node);
 		}
                 /* Food sensory neurons. */
 		for (int i = 0; i < Agent.configNumSegments; i++){//Food
 			NeatNode node = NeatNode.createRSNeatNode(total++);
-			nodes.add(node);
+			visualNodes.add(node);
 		}
                 /* Wall sensory neurons. */
 		for (int i = 0; i < Agent.configNumSegments; i++){//Wall
 			NeatNode node = NeatNode.createRSNeatNode(total++);
-			nodes.add(node);
+			visualNodes.add(node);
 		}
-                /* Enemey sensory neurons. */
+                /* Enemy sensory neurons. */
 		for (int i = 0; i < Agent.configNumSegments; i++){//Enemy
 			NeatNode node = NeatNode.createRSNeatNode(total++);
-			nodes.add(node);
+			visualNodes.add(node);
 		}
 		
-		return new Genome(new Gene[0], nodes, 0, null, null);
+		nodes.addAll(motorNodes);
+		nodes.addAll(visualNodes);
+		
+		/*
+		Full connectivity - for every sensory neuron, connect it to each
+		motor neuron.
+		*/
+		for (NeatNode vn : visualNodes) {
+			for (NeatNode mn : motorNodes) {
+				int preID = vn.getId();
+				int postID = mn.getId();
+				Gene g = new Gene(total++, vn, mn, 30.0, 1);
+				edges.add(g);
+			}
+		}
+		
+		return new Genome(edges.toArray(new Gene[0]), nodes, 0, null, null);
 	}
 
 	protected void addShark(Point2D.Double coords, int heading){
