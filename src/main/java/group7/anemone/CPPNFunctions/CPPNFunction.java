b573024fb05846f964f98d23f595f6ad3fbda91e
SebCompski <gate_tremor@msn.com>
2014-02-16 14:31:23 +0000

    Framework for CPPNFunction with Parabola function

--- src/main/java/group7/anemone/CPPNFunctions/CPPNFunction.java
+++ src/main/java/group7/anemone/CPPNFunctions/CPPNFunction.java
@@ -1,10 +1,33 @@
 package group7.anemone.CPPNFunctions;
 
 public class CPPNFunction {
+	//This function can act as any possible sigmoid function.
+	
 	public int type;
 	
-	public CPPNFunction(int newType) {
+	private double ParaA;
+	private double ParaB;
+	private double ParaC;
+	
+	public CPPNFunction(int newType, double ina, double inb, double inc) {
 		type = newType;
+		
+		switch(type) {
+		case(0):
+			ParaA = ina;
+			ParaB = inb;
+			ParaC = inc;
+		}
+		
 	}
 
+	public double calculate(double x, double y) {
+		double result=0;
+		switch(type) {
+			case(0):
+				result = (ParaA*Math.pow(x, 2))+(ParaB*x)+ParaC; 
+		}
+		
+		return result;
+	}
 }
--- src/main/java/group7/anemone/Genetics/CPPNNode.java
+++ src/main/java/group7/anemone/Genetics/CPPNNode.java
@@ -1,5 +1,27 @@
 package group7.anemone.Genetics;
 
+import group7.anemone.CPPNFunctions.CPPNFunction;
+
 public class CPPNNode {
+	CPPNFunction funcs;
+	int id;
+
+	public CPPNNode(int id, int type, double ina, double inb, double inc) {
+		this.funcs = new CPPNFunction(type, ina, inb, inc);
+		this.id = id;
+	}
+	
+	public double runCalc(double x, double y) {
+		return funcs.calculate(x, y);
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	@Override
+	public String toString() {
+		return ""+this.id;
+	}
 
 }
